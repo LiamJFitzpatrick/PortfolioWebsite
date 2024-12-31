0eb69fa0ca1e79f964f59ca767f0e42cb4a2f8eb
Liam Fitzpatrick <liam.fitzpatrick@live.com>
2024-12-31 17:38:15 -0500

    adding some content into timeline

--- website/src/app.rs
+++ website/src/app.rs
@@ -71,17 +71,38 @@ fn HomePage() -> impl IntoView {
 
         <div id="p-e-content" class="min-h-screen">
             
-            <div id="p-e-1" class="border border-4 m-auto mt-4 rounded w-1/2 h-fit bg-[#0C120C]">
-                <div class="ml-8">
+            <div id="p-e-1" class="border border-4 m-auto mt-4 rounded w-3/5 h-fit bg-[#0C120C] flex flex-row justify-around p-4">
+                <div class="flex flex-col justify-center space-y-2">
+                    <img src="/NGC-logo-white-on-clear.webp" class="max-w-1/4" />
                     <H2>"Modeling Simulation & Analysis Engineer"</H2>
                     <H3>"Northrop Grumman Corporation"</H3>
-                    <P>plop</P>
+                </div>
+                <div class="space-y-2">
+                    <P>"Developed a high-fidelity C++ simulation of air-to-air radars for Northrop Grumman’s Emerging Capabilities division."</P>
+                    <P>"Lead software development process of C++ simulation for a team of 6 engineers."</P>
+                    <P>"Created tooling to analyze IQ data, determining angle of arrival, range, and velocity of returns."</P>
+                    <P>"Contributed to technical documentation process – Model Design Review (MDR) – at all stages."</P>
+                    <P>"Developed a web application – PyCAM – to assist control account managers in tracking costs within programs."</P>
+                    <P>"Additionally: recognized as Top Performer, mentored & trained other engineers"</P>
                 </div>
             </div>
             <div class="my-16 mx-auto w-fit bg-[#0C120C]">
                 <p>2023</p>
             </div>
-            <div id="p-e-2" class="border border-4 m-auto rounded w-3/5 h-32 bg-[#0C120C]">
+            <div id="p-e-2" class="border border-4 m-auto mt-4 rounded w-3/5 h-fit bg-[#0C120C] flex flex-row justify-around p-4">
+                <div class="flex flex-col justify-center max-w-1/4">
+                    <img src="/Ingredion_Logo_WebUseOnly_500.png" />
+                    <H2>"Process Lead / Process Engineer"</H2>
+                    <H3>"Ingredion Incorporated"</H3>
+                </div>
+                <div>
+                    <P>"Developed a high-fidelity C++ simulation of air-to-air radars for Northrop Grumman’s Emerging Capabilities division."</P>
+                    <P>"Lead software development process of C++ simulation for a team of 6 engineers."</P>
+                    <P>"Created tooling to analyze IQ data, determining angle of arrival, range, and velocity of returns."</P>
+                    <P>"Contributed to technical documentation process – Model Design Review (MDR) – at all stages."</P>
+                    <P>"Developed a web application – PyCAM – to assist control account managers in tracking costs within programs."</P>
+                    <P>"Additionally: recognized as Top Performer, mentored & trained other engineers"</P>
+                </div>
             </div>
             <div class="my-16 mx-auto w-fit bg-[#0C120C]">
                 <p>2021</p>
